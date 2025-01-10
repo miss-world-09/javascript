a8d0a3361cdb79ee7e37cab2a72c339a1721fb62
Disha <disha.sahay2005@gmail.com>
2025-01-10 20:25:30 +0530

    CH3 NOTES

---
+++ chapter3.js
@@ -0,0 +1,34 @@
+// LOOPS AND FUNCTIONS:- WE USE LOOPS TO PERFORM REPETITIVE ACTIONS
+// TYPES OF LOOPS:- 1. FOR LOOP:-
+// for(statement1,statement2,statement3){
+//code to be executed
+// }
+
+// let a=1;
+// console.log(a+1);
+// console.log(a+2);
+// console.log(a+3);
+
+// for(i=1;i<40;i++){
+//     console.log(a+i);
+// }
+
+let obj={
+    name:"riya",
+    role:"student",
+    school:"ssms",
+}
+
+// let i = 0;
+// while (i<60000) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
+
+
