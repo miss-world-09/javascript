f689cce3b6f0fec2ef6f3b5eef3769b5226daa82
Disha <disha.sahay2005@gmail.com>
2025-02-09 18:41:50 +0530

    CH 10 NOTES

--- chapter10.js
+++ chapter10.js
@@ -53,3 +53,27 @@ const fruiitss = ['apple', 'orange'];
 // Inserting 'banana' at index 1
 fruiitss.splice(1, 0, 'banana');
 console.log(fruiitss); 
+
+// LOOPS:-
+// 1. FOR EACH LOOP:- WE CAN ACCESS EACH AND EVERY ELEMENT.
+// let arr=[1,3,2,6,7,3];
+// arr.forEach((value,index)=> {
+//     console.log("number",value,"index",index);
+// });
+// TRADITIONAL LOOP
+// for(let value of arr){
+//     console.log(value);
+// }
+
+// ARRAY WITH FUNCTION:
+let arr=[1,3,5,7,3];
+function getsum(arr){
+    let sum=0;
+    arr.forEach((value)=> {
+        sum=sum+value;
+    });
+    return sum;
+}
+let totalsum=getsum(arr);
+console.log(totalsum);
+
