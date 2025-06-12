5e4da8e7ba897ec96d9e904a55d43bfd7f44ebbc
miss-world-09 <disha.sahay2005@gmail.com>
2025-06-13 01:07:33 +0530

    Update chapter2.js

--- chapter2.js
+++ chapter2.js
@@ -1,19 +1,31 @@
 // EXPRESSION AND CONDITIONAL:-
 
-// OPERATORS IN JS:-
-// 1. ARITHMETIC OPERATORS:- EXAMPLE:
-// +,-,/,%,**  EXPONENTIATION,++ INCREMENT, -- DECREMENT
+/* OPERATORS IN JS:-
+ 1. ARITHMETIC OPERATORS:- EXAMPLE:
+ +,-,/,%,**  EXPONENTIATION,++ INCREMENT, -- DECREMENT
+ let addition=4+5;
+ let subtract=5-3;
+ let mult=5*8;
+ let divide=8/4;
+ let reminder=5%5;
+ let expo=2**3;
+*/
 
-// 2. ASSIGNMENT OPERATOR:- EX:
-// =  (X=Y), +=(X=X+Y), -=(X=X-Y), *=(X=X*Y), /=(X=X/Y), %=(X=X%Y), **=(X=X**Y)
+/* 2. ASSIGNMENT OPERATOR:- EX:
+ =  (X=Y), +=(X=X+Y), -=(X=X-Y), *=(X=X*Y), /=(X=X/Y), %=(X=X%Y), **=(X=X**Y)
 
+ let score=10+11;
+ let num=10;
+ num+=5; (num=num+5)
+ console.log(num);
+*/
 // 3.COMPARISON OPERATORS:- EX:
 // ==->EQUALS TO, != ->NOT EQUALS TO, === ->EQUAL VALUE AND TYPE
 // !== ->NOT EQUAL VALUE OR TYPE, > ->GREATER THAN, < ->LESS THAN
 // >= ->GREATER THAN OR EQUAL TO, <= ->LESS THAN OR EQUAL TO, ?->TERNARY OPERATOR
 
 // 4. LOGICAL OPERATORS:- EX:
-// &&->LOGICAL AND, ||->LOGICAL OR, !->LOGICAL NOT
+// &&->LOGICAL AND, ||->LOGICAL OR, !->LOGICAL NOT OR REVERSE
 // CONDITIONAL STATEMENTS:- IF,IF-ELSE,IF-IF ELSE-ELSE STATEMENT
 age=18
 if(age>0){
@@ -34,3 +46,7 @@ else{
 a=6
 b=5
 let c=a>b?(a-b):(b-a);
+
+/*precendence:
+let score=((2*(3+2))-1);
+console.log(score);
