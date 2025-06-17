63c8eab3ff956931a0b3c476026ca5bdbb3e25b8
miss-world-09 <disha.sahay2005@gmail.com>
2025-06-17 18:39:43 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,11 @@
+// WRITE A FOR LOOP THAT LOOPS THROUGH THE ARRAY ["GREEN TEA","BLACK TEA","CHAI","OOLONG TEA"] AND STOPS THE LOOP WHEN IT FINDS "CHAI". STORE ALL TEA BEFORE CHAI IN AN NEW ARRAY NAMED 'SELECTED TEA'.
+let tea=["green tea","black tea","chai","oolong tea"];
+let i;
+let selectedtea=[];
+for(i=0;i<=tea.length;i++){
+    if(tea[i]==="chai"){
+        break;
+    }
+    selectedtea.push(tea[i]);
+}
+console.log(selectedtea);
