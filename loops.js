4c5efdc858977e0d63c6d19a2efa3c817d47fa4a
miss-world-09 <disha.sahay2005@gmail.com>
2025-06-18 19:51:45 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -44,3 +44,69 @@ for(let t of tea){
 console.log(preferredteas);
 
 
+
+/*USE A FOR IN LOOP TO LOOP THROUGH AN OBJECT CONATAINING CITY POPULATION. STOPE THE LOOP WHEN POPULATION OF BERLIN IS FOUND AND STORE ALL PREVIOUS CITIES POPULATION IN A NEW OBJECT NAMED 'CITYPOPULATIONS'.
+LET CITIESPOPULATION={
+    "LONDON": 89000,
+    "NEW YORK":84000,
+    "PARIS":22000,
+    "BERLIN":35000,
+}
+*/
+let citiespopulation={
+    "london":89000,
+    "new york":84000,
+    "paris":22000,
+    "berlin":35000
+};
+let citypopulations={};
+for(let city in citiespopulation){
+    if(city==="berlin"){
+        break;
+    }
+    citypopulations[city]=citiespopulation[city];
+}
+console.log(citypopulations);
+
+
+/* USE A FOR IN LOOP TO LOOP THROUGH AN OBJECT CONTAINING CITY POPULATIONS. SKIP ANY CITY WITH POPULATION BELOW 3 MILLION AND STORE THE REST IN A NEW OBJECT NAMED 'LARGECITIES'
+LET WORLDCITIES={
+    "SYDNEY":50000,
+    "TOKYO":90000,
+    "BERLIN":35000,
+    "PARIS":22000
+};
+*/
+let worldcities={
+    "Sydney":50000,
+    "Tokyo":90000,
+    "Berlin":35000,
+    "Paris":22000
+};
+let largecities={};
+for(let city in worldcities){
+    if(worldcities[city]<30000){
+        continue;
+    }
+    largecities[city]=worldcities[city]
+}
+console.log(largecities);
+
+/* FOR EACH LOOP:- The forEach() loop is a method used to iterate over each element in an array and perform an action on them.
+SYNTAX:
+array.forEach(function(element, index, array) {
+  // do something with element
+});
+element: current item in the array
+index (optional): position of the item
+array (optional): the whole array
+EXAMPLE:- 
+let fruits = ["apple", "banana", "mango"];
+
+fruits.forEach(function(fruit) {
+  console.log(fruit);
+});
+*/
+
+
+
