706e1596875c4f2d9636502e8044b9c659b9cc99
Disha <disha.sahay2005@gmail.com>
2025-06-28 22:36:32 +0530

    module

---
+++ appM.js
@@ -0,0 +1,10 @@
+//Module import file
+
+//default import
+import multiply from "./mathoperationsM";
+
+//named import
+import { add,subtract } from "./mathoperationsM";
+
+console.log(multiply(2,2));
+console.log(add(5,3));
